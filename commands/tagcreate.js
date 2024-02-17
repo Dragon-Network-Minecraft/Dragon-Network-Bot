4f5f44c4dbfa902715e557f1a153e820cea38886
G9 Aerospace <g9militants@gmail.com>
2024-02-17 19:56:19 +0530

    🛠️ .gitignore -> Added data/tags/ to ignore list

---
+++ commands/tagcreate.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const logger = require('../utilities/logger');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('tagcreate')
+    .setDescription('Create a new tag')
+    .addStringOption(option => 
+      option.setName('tagname')
+        .setDescription('Name of the tag')
+        .setRequired(true)
+    )
+    .addStringOption(option => 
+      option.setName('tagcontent')
+        .setDescription('Content of the tag')
+        .setRequired(true)
+    ),
+
+  async execute(interaction) {
+    try {
+      const tagName = interaction.options.getString('tagname');
+      const tagContent = interaction.options.getString('tagcontent');
+      const tagPath = path.join(__dirname, '..', 'data', 'tags', `${tagName.toLowerCase()}.json`);
+
+      // Check if the tag already exists
+      if (fs.existsSync(tagPath)) {
+        return interaction.reply({ content: 'Tag with that name already exists!', ephemeral: true });
+      }
+
+      // Create the tag file
+      fs.writeFileSync(tagPath, JSON.stringify({ content: tagContent }));
+
+      logger.log(`Tag created: ${tagName} by ${interaction.user.tag}`);
+
+      await interaction.reply(`Tag created successfully: ${tagName}`);
+    } catch (error) {
+      logger.error(`Error creating tag for ${interaction.user.tag}: ${error}`);
+      await interaction.reply({ content: 'There was an error while creating the tag!', ephemeral: true });
+    }
+  },
+};
